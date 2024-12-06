cfccbff235ee1c69644d8978f63ee49dfe379cc8
STerliakov <terlya.stas@gmail.com>
2024-12-06 23:56:42 +0100

    Store visits information inline, ignore unreachable cells

--- days/day6.c
+++ days/day6.c
@@ -10,30 +10,42 @@
 // can't be const size_t: "VLA can't be initialized"
 #define DIRECTIONS_COUNT 4
 
-bool find_start(board_t *board, point_t *start) {
-    char *i_start = strchr(board->body, '^');
-    if (i_start == NULL) {
-        return false;
+const char MOVE_MASK = (1 << DIRECTIONS_COUNT) - 1;
+const char EMPTY = 1 << DIRECTIONS_COUNT;
+const char WALL = 1 << (DIRECTIONS_COUNT + 1);
+
+bool board_translate(board_t *board, point_t *start) {
+    size_t offset = 0;
+    size_t board_size = board->width * board->height;
+    for (char *ch = board->body + board_size - 1; ch >= board->body; ch--) {
+        switch (*ch) {
+            case '^':
+                offset = ch - board->body;
+                // fallthrough
+            case '.':
+                *ch = EMPTY;
+                break;
+            case '#':
+                *ch = WALL;
+                break;
+        }
     }
-    size_t offset = (size_t)(i_start - board->body);
     size_t r = offset / board->width, c = offset % board->width;
     *start = (point_t){r, c};
     return true;
 }
 
-long traverse(board_t *board, board_t *visits, point_t pos) {
-    assert(board->width == visits->width);
-    assert(board->height == visits->height);
-
-    offset_t directions[DIRECTIONS_COUNT] = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+long traverse(board_t *board, point_t pos) {
+    static offset_t directions[DIRECTIONS_COUNT]
+        = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
     size_t dir_index = 0;
-
-    long total = 0;
     offset_t dir = directions[dir_index];
     char dir_flag = (char)(1 << dir_index);
+
+    long total = 0;
     char ch;
     while ((ch = board_at(board, pos)) != 0) {
-        if (ch == '#') {
+        if (ch & WALL) {
             pos.c -= dir.dc;
             pos.r -= dir.dr;
             dir_index = (dir_index + 1) % DIRECTIONS_COUNT;
@@ -41,14 +53,13 @@ long traverse(board_t *board, board_t *visits, point_t pos) {
             dir_flag = (char)(1 << dir_index);
             continue;
         }
-        char old_visit = board_at(visits, pos);
-        if (old_visit == 0) {
+        if ((ch & MOVE_MASK) == 0) {
             total++;
-        } else if (old_visit & dir_flag) {
+        } else if (ch & dir_flag) {
             // cycle found - been there in the same direction
             return -1;
         }
-        board_set(visits, pos, (char)(dir_flag | old_visit));
+        board_set(board, pos, (char)(dir_flag | ch));
         pos.c += dir.dc;
         pos.r += dir.dr;
     }
@@ -63,24 +74,15 @@ long part1(FILE *input) {
     }
 
     point_t start;
-    if (!find_start(&board, &start)) {
+    if (!board_translate(&board, &start)) {
         fprintf(stderr, "Start symbol (^) not found\n");
         board_delete(&board);
         return -1;
     }
 
-    board_t visits = {NULL, board.height, board.width};
-    visits.body = calloc(sizeof(char), board.width * board.height);
-    if (visits.body == NULL) {
-        perror("Failed to allocate memory for visits");
-        board_delete(&board);
-        return -1;
-    }
-
-    long total = traverse(&board, &visits, start);
+    long total = traverse(&board, start);
 
     board_delete(&board);
-    board_delete(&visits);
     return total;
 }
 
@@ -93,43 +95,47 @@ long part2(FILE *input) {
     size_t board_size = board.width * board.height;
 
     point_t start;
-    if (!find_start(&board, &start)) {
+    if (!board_translate(&board, &start)) {
         fprintf(stderr, "Start symbol (^) not found\n");
         board_delete(&board);
         return -1;
     }
 
-    board_t visits = {NULL, board.height, board.width};
-    visits.body = calloc(sizeof(char), board_size);
-    if (visits.body == NULL) {
-        perror("Failed to allocate memory for visits");
-        board_delete(&board);
-        return -1;
-    }
-
-    board_t board_copy = {NULL, board.height, board.width};
+    board_t board_copy = board;
     board_copy.body = calloc(sizeof(char), board_size);
     if (board_copy.body == NULL) {
         perror("Failed to allocate memory for a board copy");
         board_delete(&board);
-        board_delete(&visits);
         return -1;
     }
+    memcpy(board_copy.body, board.body, board_size * sizeof(char));
+
+    traverse(&board_copy, start);
+    char *orig_visits = calloc(sizeof(char), board_size);
+    if (orig_visits == NULL) {
+        perror("Failed to allocate memory for a board copy");
+        board_delete(&board);
+        board_delete(&board_copy);
+        return -1;
+    }
+    memcpy(orig_visits, board_copy.body, board_size * sizeof(char));
 
     long total = 0;
+    size_t start_index = start.r * board.width + start.c;
     for (size_t i = 0; i < board_size; i++) {
-        if (board.body[i] != '.') {
+        if (board.body[i] != EMPTY || i == start_index
+            || (orig_visits[i] & MOVE_MASK) == 0) {
+            // Skip if there's already an obstacle or the cell is unreachable
             continue;
         }
         memcpy(board_copy.body, board.body, board_size * sizeof(char));
-        board_copy.body[i] = '#';
-        memset(visits.body, 0, board_size * sizeof(char));
-        if (traverse(&board_copy, &visits, start) == -1)
+        board_copy.body[i] = WALL;
+        if (traverse(&board_copy, start) == -1)
             total++;
     }
 
     board_delete(&board);
-    board_delete(&visits);
     board_delete(&board_copy);
+    free(orig_visits);
     return total;
 }
