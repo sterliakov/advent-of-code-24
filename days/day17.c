28b2ec0f20b761f0d325ad396047cb336d8894ae
STerliakov <terlya.stas@gmail.com>
2024-12-17 21:50:29 +0100

    Rewrite with generic vec type

--- days/day17.c
+++ days/day17.c
@@ -2,12 +2,14 @@
 #include <limits.h>
 #include <stdbool.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include "common.h"
 
-typedef long long value_t;
+#include "../lib/vec.h"
+
+typedef unsigned long long value_t;
 typedef unsigned char op_t;
+
+typedef VEC_OF(op_t) op_vec;
 
 typedef struct __attribute__((aligned(32))) state_t {
     value_t a;
@@ -15,12 +17,12 @@ typedef struct __attribute__((aligned(32))) state_t {
     value_t c;
 } state_t;
 
-size_t read_input(FILE *input, state_t *state, op_t **ops) {
-    if (fscanf(input, "Register A: %lld\n", &(*state).a) < 1
-        || fscanf(input, "Register B: %lld\n", &(*state).b) < 1
-        || fscanf(input, "Register C: %lld\n", &(*state).c) < 1) {
+bool read_input(FILE *input, state_t *state, op_vec *ops) {
+    if (fscanf(input, "Register A: %llu\n", &(*state).a) < 1
+        || fscanf(input, "Register B: %llu\n", &(*state).b) < 1
+        || fscanf(input, "Register C: %llu\n", &(*state).c) < 1) {
         fprintf(stderr, "Malformed input\n");
-        return 0;
+        return false;
     }
 
 // Just discard a decorative prefix
@@ -29,33 +31,12 @@ size_t read_input(FILE *input, state_t *state, op_t **ops) {
     fscanf(input, "\nProgram: ");
 #pragma GCC diagnostic pop
 
-    size_t count = 0;
-    size_t arr_size = 2;
-    *ops = malloc(arr_size * sizeof(op_t));
-    if (*ops == NULL) {
-        perror("Malloc failed");
-        return 0;
+    op_t x, y;
+    while (fscanf(input, "%c,%c,", &x, &y) == 2) {
+        VEC_PUSH(*ops, x - '0');
+        VEC_PUSH(*ops, y - '0');
     }
-    while (true) {
-        op_t x, y;
-        if (fscanf(input, "%c,%c,", &x, &y) < 2) {
-            break;
-        }
-        while (count + 1 >= arr_size) {
-            arr_size *= 2;
-            op_t *olddest = *ops;
-            *ops = realloc(*ops, arr_size * sizeof(op_t));
-            if (*ops == NULL) {
-                free(olddest);
-                perror("Malloc failed");
-                return 0;
-            }
-        }
-        (*ops)[count] = x - '0';
-        (*ops)[count + 1] = y - '0';
-        count += 2;
-    }
-    return count;
+    return (*ops).size > 0;
 }
 
 value_t opval(state_t state[const static 1], op_t operand) {
@@ -81,14 +62,13 @@ value_t adiv(value_t a, value_t val) {
 
 bool run(
     state_t state[static 1],
-    op_t ops[const static 1],
-    size_t opcount,
+    op_vec ops[const static 1],
     bool (*out)(op_t, void *),
     void *out_state
 ) {
-    for (size_t i = 0; i < opcount; i += 2) {
-        op_t op = ops[i];
-        op_t operand = ops[i + 1];
+    for (size_t i = 0; i < (*ops).size; i += 2) {
+        op_t op = VEC_AT(*ops, i);
+        op_t operand = VEC_AT(*ops, i + 1);
         value_t val = opval(state, operand);
         switch (op) {
             case 0:
@@ -125,70 +105,48 @@ bool run(
     return true;
 }
 
-typedef struct __attribute__((aligned(32))) out_vec_t {
-    op_t *vec;
-    size_t alloc;
-    size_t count;
-} out_vec_t;
-
 bool out1(op_t val, void *out_state) {
-    out_vec_t *state = out_state;
-    if (state->count >= state->alloc) {
-        state->alloc *= 2;
-        op_t *old = state->vec;
-        state->vec = realloc(state->vec, state->alloc * sizeof(op_t));
-        if (state->vec == NULL) {
-            free(old);
-            perror("realloc failed");
-            return false;
-        }
-    }
-    state->vec[state->count++] = val;
+    op_vec *state = out_state;
+    VEC_PUSH(*state, val);
     return true;
 }
 
 long part1(FILE *input) {
-    out_vec_t out_state = {NULL, 4, 0};
-    out_state.vec = malloc(out_state.alloc * sizeof(op_t));
-    if (out_state.vec == NULL) {
-        perror("malloc failed");
-        return -1;
-    }
-
     state_t state = {0, 0, 0};
-    op_t *ops = NULL;
-    size_t count = read_input(input, &state, &ops);
-    if (count == 0) {
-        free(out_state.vec);
-        free(ops);
+    op_vec ops;
+    VEC_NEW(ops);
+    if (!read_input(input, &state, &ops)) {
+        VEC_DELETE(ops);
         return -1;
     }
 
+    op_vec out_state;
+    VEC_NEW(out_state);
     long code = 0;
-    if (run(&state, ops, count, out1, &out_state)) {
-        assert(out_state.count > 0);
-        printf("%c", out_state.vec[0] + '0');
-        for (size_t i = 1; i < out_state.count; i++) {
-            printf(",%c", out_state.vec[i] + '0');
+    if (run(&state, &ops, out1, &out_state)) {
+        assert(out_state.size > 0);
+        printf("%c", VEC_AT(out_state, 0) + '0');
+        for (size_t i = 1; i < out_state.size; i++) {
+            printf(",%c", VEC_AT(out_state, i) + '0');
         }
         putc('\n', stdout);
     } else {
         code = -1;
     }
-    free(out_state.vec);
-    free(ops);
+    VEC_DELETE(out_state);
+    VEC_DELETE(ops);
     return code;
 }
 
-typedef struct __attribute__((aligned(32))) out_check_t {
-    op_t *ops;
-    size_t opcount;
+typedef struct __attribute__((aligned(64))) out_check_t {
+    op_vec ops;
     size_t tail;
 } out_check_t;
 
 bool out2(op_t val, void *out_state) {
     out_check_t *state = out_state;
-    if (state->tail == 0 || state->ops[state->opcount - state->tail] != val % 8)
+    if (state->tail == 0
+        || VEC_AT(state->ops, state->ops.size - state->tail) != val % 8)
         return false;
     state->tail--;
     return true;
@@ -196,21 +154,21 @@ bool out2(op_t val, void *out_state) {
 
 long part2(FILE *input) {
     state_t state0 = {0, 0, 0};
-    op_t *ops = NULL;
-    size_t count = read_input(input, &state0, &ops);
-    if (count == 0) {
-        free(ops);
+    op_vec ops;
+    VEC_NEW(ops);
+    if (!read_input(input, &state0, &ops)) {
+        VEC_DELETE(ops);
         return -1;
     }
 
     size_t tail = 1;
-    out_check_t out_state = {ops, count, 1};
+    out_check_t out_state = {ops, 1};
     value_t a = 0;
     while (true) {
         out_state.tail = tail;
         state_t state = {.a = a, .b = state0.b, .c = state0.c};
-        if (run(&state, ops, count, out2, &out_state) && out_state.tail == 0) {
-            if (tail == count) {
+        if (run(&state, &ops, out2, &out_state) && out_state.tail == 0) {
+            if (tail == ops.size) {
                 break;
             } else {
                 tail++;
@@ -220,6 +178,6 @@ long part2(FILE *input) {
             a++;
         }
     }
-    free(ops);
+    VEC_DELETE(ops);
     return (long)a;
 }
---
+++ lib/vec.h
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Can't wrap type name in parens here
+// NOLINTBEGIN(bugprone-macro-parentheses)
+#define VEC_OF(typ)                       \
+    struct __attribute__((aligned(32))) { \
+        typ *data;                        \
+        size_t size;                      \
+        size_t capacity;                  \
+    }
+// NOLINTEND(bugprone-macro-parentheses)
+
+#define VEC_WITH_CAPACITY(vec, n)                       \
+    do {                                                \
+        (vec).data = malloc((n) * sizeof(*(vec).data)); \
+        if (!(vec).data) {                              \
+            fputs("malloc failed!\n", stderr);          \
+            abort();                                    \
+        }                                               \
+        (vec).size = 0;                                 \
+        (vec).capacity = (n);                           \
+    } while (0)
+
+#define VEC_NEW(vec) VEC_WITH_CAPACITY(vec, 1)
+
+#define VEC_PUSH(vec, val)                                                   \
+    do {                                                                     \
+        if ((vec).size >= (vec).capacity) {                                  \
+            (vec).capacity *= 2;                                             \
+            void *new_data                                                   \
+                = realloc((vec).data, (vec).capacity * sizeof(*(vec).data)); \
+            if (!new_data) {                                                 \
+                fprintf(stderr, "realloc failed\n");                         \
+                exit(1);                                                     \
+            }                                                                \
+            (vec).data = new_data;                                           \
+        }                                                                    \
+        (vec).data[(vec).size++] = (val);                                    \
+    } while (0)
+
+#define VEC_AT(vec, i) (vec).data[i]
+
+#define VEC_LAST(vec) (vec).data[(vec).size - 1]
+
+#define VEC_DELETE(vec)     \
+    do {                    \
+        free((vec).data);   \
+        (vec).size = 0;     \
+        (vec).capacity = 0; \
+    } while (0)
