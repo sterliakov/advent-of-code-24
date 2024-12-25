a271fdfed07de4f81857ff72def5b962ad699dee
STerliakov <terlya.stas@gmail.com>
2024-12-25 16:24:13 +0100

    Cheap perf improvement in day22

--- days/day22.c
+++ days/day22.c
@@ -1,15 +1,16 @@
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "common.h"
 
-inline long mix(long a, long b) {
+inline uint_fast32_t mix(uint_fast32_t a, uint_fast32_t b) {
     return a ^ b;
 }
-inline long prune(long v) {
+inline uint_fast32_t prune(uint_fast32_t v) {
     return v % 16777216;
 }
-inline long nxt(long v) {
+inline uint_fast32_t nxt(uint_fast32_t v) {
     v = prune(mix(v, v << 6));
     v = prune(mix(v, v >> 5));
     v = prune(mix(v, v << 11));
@@ -17,16 +18,16 @@ inline long nxt(long v) {
 }
 
 long part1(FILE *input) {
-    long total = 0;
-    long v;
-    while (fscanf(input, "%ld\n", &v) == 1) {
+    uint_fast32_t total = 0;
+    uint_fast32_t v;
+    while (fscanf(input, "%lu\n", &v) == 1) {
         for (size_t i = 0; i < 2000; i++) {
             v = nxt(v);
         }
         total += v;
     }
 
-    return total;
+    return (long)total;
 }
 
 long part2(FILE *input) {
@@ -34,8 +35,8 @@ long part2(FILE *input) {
     const size_t diff_len = diff_count * diff_count * diff_count * diff_count;
     const size_t iters = 2000;
 
-    long *totals = calloc(diff_len, sizeof(long));
-    char *diff = calloc(iters + 1, sizeof(char));
+    uint_fast32_t *totals = calloc(diff_len, sizeof(uint_fast32_t));
+    int_fast8_t *diff = calloc(iters + 1, sizeof(int_fast8_t));
     size_t *visited = calloc(diff_len, sizeof(size_t));
     if (totals == NULL || diff == NULL || visited == NULL) {
         free(totals);
@@ -45,16 +46,16 @@ long part2(FILE *input) {
         return -1;
     }
 
-    long v;
+    uint_fast32_t v;
     // Save on memset - instead of resetting visits, just check for new value
     // every time
     size_t id = 1;
-    while (fscanf(input, "%ld\n", &v) == 1) {
-        long price = v % 10, oldprice = price;
+    while (fscanf(input, "%lu\n", &v) == 1) {
+        uint_fast8_t price = v % 10, oldprice = price;
         for (size_t i = 1; i <= iters; i++) {
             v = nxt(v);
             price = v % 10;
-            diff[i] = (char)(price - oldprice + 9);
+            diff[i] = (int_fast8_t)(price - oldprice + 9);
             if (i >= 4) {
                 size_t k = (diff[i - 3]) * diff_count * diff_count * diff_count
                            + (diff[i - 2]) * diff_count * diff_count
@@ -71,7 +72,7 @@ long part2(FILE *input) {
     free(diff);
     free(visited);
 
-    long best = 0;
+    uint_fast32_t best = 0;
     for (size_t i = 0; i < diff_len; i++) {
         if (totals[i] > best) {
             best = totals[i];
@@ -79,5 +80,5 @@ long part2(FILE *input) {
     }
     free(totals);
 
-    return best;
+    return (long)best;
 }
